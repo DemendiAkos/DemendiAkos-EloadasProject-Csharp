b4a3e2c0bc0e500b5aef11a508d79bec6cbfb211
DemendiAkos <akosdemedni@gmail.com>
2024-06-05 16:12:50 +0200

    feat: finished tests

--- TestEloadasProject/EloadasTests.cs
+++ TestEloadasProject/EloadasTests.cs
@@ -9,7 +9,7 @@ namespace EloadasProject.Tests
         [Test]
         public void Constructor_ShouldThrowArgumentException_WhenRowOrSeatCountIsZeroOrNegative()
         {
-            // Arrange, Act & Assert
+            
             Assert.Throws<ArgumentException>(() => new Eloadas(0, 10));
             Assert.Throws<ArgumentException>(() => new Eloadas(10, 0));
             Assert.Throws<ArgumentException>(() => new Eloadas(-1, 10));
@@ -19,10 +19,10 @@ namespace EloadasProject.Tests
         [Test]
         public void Constructor_ShouldInitializeCorrectly_WhenRowAndSeatCountArePositive()
         {
-            // Arrange & Act
+            
             var eloadas = new Eloadas(10, 10);
 
-            // Assert
+           
             Assert.NotNull(eloadas);
             Assert.AreEqual(100, eloadas.SzabadHelyek);
             Assert.IsFalse(eloadas.Teli);
@@ -31,13 +31,13 @@ namespace EloadasProject.Tests
         [Test]
         public void Lefoglal_ShouldReturnTrue_WhenThereAreFreeSeats()
         {
-            // Arrange
+           
             var eloadas = new Eloadas(2, 2);
 
-            // Act
+           
             var result = eloadas.Lefoglal();
 
-            // Assert
+            
             Assert.IsTrue(result);
             Assert.AreEqual(3, eloadas.SzabadHelyek);
         }
@@ -45,65 +45,65 @@ namespace EloadasProject.Tests
         [Test]
         public void Lefoglal_ShouldReturnFalse_WhenThereAreNoFreeSeats()
         {
-            // Arrange
+            
             var eloadas = new Eloadas(1, 1);
             eloadas.Lefoglal();
 
-            // Act
+           
             var result = eloadas.Lefoglal();
 
-            // Assert
+           
             Assert.IsFalse(result);
         }
 
         [Test]
         public void Teli_ShouldReturnTrue_WhenAllSeatsAreBooked()
         {
-            // Arrange
+            
             var eloadas = new Eloadas(1, 1);
             eloadas.Lefoglal();
 
-            // Act
+            
             var isFull = eloadas.Teli;
 
-            // Assert
+           
             Assert.IsTrue(isFull);
         }
 
         [Test]
         public void Foglalt_ShouldReturnTrue_WhenSeatIsBooked()
         {
-            // Arrange
+          
             var eloadas = new Eloadas(1, 1);
             eloadas.Foglal(0, 0);
 
-            // Act
+           
             var isBooked = eloadas.Foglalt(1, 1);
 
-            // Assert
+          
             Assert.IsTrue(isBooked);
         }
 
         [Test]
         public void Foglalt_ShouldReturnFalse_WhenSeatIsNotBooked()
         {
-            // Arrange
+        
             var eloadas = new Eloadas(1, 1);
 
-            // Act
+           
             var isBooked = eloadas.Foglalt(1, 1);
 
-            // Assert
+            
             Assert.IsFalse(isBooked);
         }
 
         [Test]
         public void Foglalt_ShouldThrowArgumentException_WhenRowOrSeatIsInvalid()
         {
-            // Arrange
+            
             var eloadas = new Eloadas(1, 1);
 
-            // Act & Assert
+           
             Assert.Throws<ArgumentException>(() => eloadas.Foglalt(0, 1));
             Assert.Throws<ArgumentException>(() => eloadas.Foglalt(1, 0));
             Assert.Throws<ArgumentException>(() => eloadas.Foglalt(2, 1));
@@ -113,14 +113,14 @@ namespace EloadasProject.Tests
         [Test]
         public void SzabadHelyek_ShouldReturnCorrectCount()
         {
-            // Arrange
+       
             var eloadas = new Eloadas(2, 2);
 
-            // Act
-            eloadas.Lefoglal(); // 1 seat booked
-            eloadas.Lefoglal(); // 2 seats booked
+      
+            eloadas.Lefoglal();
+            eloadas.Lefoglal();
 
-            // Assert
+            
             Assert.AreEqual(2, eloadas.SzabadHelyek);
         }
     }
